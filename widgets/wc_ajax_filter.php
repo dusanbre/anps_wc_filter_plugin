e4350a8a861dbfdf2d633fdc7852f2f98fed5add
dusanbre <dusan.antonijevic2404@gmail.com>
2021-02-28 16:17:46 +0100

    added method for price range

--- widgets/wc_ajax_filter.php
+++ widgets/wc_ajax_filter.php
@@ -5,38 +5,32 @@ class Anps_WC_Ajax_Filter_Widget extends WP_Widget {
 
 	function __construct() {
 		parent::__construct(
-		// Base ID of your widget
 			'Anps_WC_Ajax_Filter_Widget',
-			// Widget name will appear in UI
 			__( 'Anpstheme WC Ajax Filter', 'anps_wc_filter' ),
-			// Widget description
 			array( 'description' => __( 'Widget for filtering products', 'anps_wc_filter' ) )
 		);
 	}
 
 		// Creating widget front-end
-
 	public function widget( $args, $instance ) {
 		$title = apply_filters( 'widget_title', $instance['title'] );
 
-		// before and after widget arguments are defined by themes
 		echo $args['before_widget'];
 		if ( ! empty( $title ) ) {
 			echo $args['before_title'] . $title . $args['after_title'];
 		}
-
-		// This is where you run the code and display the output
 		echo __( 'Hello, World!', 'anps_wc_filter' );
 		echo $args['after_widget'];
 	}
 
 		// Widget Backend
 	public function form( $instance ) {
-		if ( isset( $instance['title'] ) ) {
-			$title = $instance['title'];
-		} else {
-			$title = __( 'New title', 'anps_wc_filter' );
-		}
+		$title       = isset( $instance['title'] ) ? $instance['title'] : '';
+		$all_attr    = wc_get_attribute_taxonomies();
+		$all_cat     = get_terms( 'product_cat' );
+		$price_range = $this->helper_get_price_range();
+		echo '<pre>';
+		print_r( $price_range );
 		// Widget admin form
 		?>
 		<p>
@@ -51,5 +45,46 @@ class Anps_WC_Ajax_Filter_Widget extends WP_Widget {
 		$instance          = array();
 		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
 		return $instance;
+	}
+
+	protected function helper_get_price_range() {
+		global $wpdb;
+
+		$args       = WC()->query->get_main_query()->query_vars;
+		$tax_query  = isset( $args['tax_query'] ) ? $args['tax_query'] : array();
+		$meta_query = isset( $args['meta_query'] ) ? $args['meta_query'] : array();
+
+		if ( ! is_post_type_archive( 'product' ) && ! empty( $args['taxonomy'] ) && ! empty( $args['term'] ) ) {
+			$tax_query[] = WC()->query->get_main_tax_query();
+		}
+
+		foreach ( $meta_query + $tax_query as $key => $query ) {
+			if ( ! empty( $query['price_filter'] ) || ! empty( $query['rating_filter'] ) ) {
+				unset( $meta_query[ $key ] );
+			}
+		}
+
+		$meta_query = new WP_Meta_Query( $meta_query );
+		$tax_query  = new WP_Tax_Query( $tax_query );
+		$search     = WC_Query::get_main_search_query_sql();
+
+		$meta_query_sql   = $meta_query->get_sql( 'post', $wpdb->posts, 'ID' );
+		$tax_query_sql    = $tax_query->get_sql( $wpdb->posts, 'ID' );
+		$search_query_sql = $search ? ' AND ' . $search : '';
+
+		$sql = "
+			SELECT min( min_price ) as min_price, MAX( max_price ) as max_price
+			FROM {$wpdb->wc_product_meta_lookup}
+			WHERE product_id IN (
+				SELECT ID FROM {$wpdb->posts}
+				" . $tax_query_sql['join'] . $meta_query_sql['join'] . "
+				WHERE {$wpdb->posts}.post_type IN ('" . implode( "','", array_map( 'esc_sql', apply_filters( 'woocommerce_price_filter_post_type', array( 'product' ) ) ) ) . "')
+				AND {$wpdb->posts}.post_status = 'publish'
+				" . $tax_query_sql['where'] . $meta_query_sql['where'] . $search_query_sql . '
+			)';
+
+		$sql = apply_filters( 'woocommerce_price_filter_sql', $sql, $meta_query_sql, $tax_query_sql );
+
+		return $wpdb->get_row( $sql );
 	}
 }
