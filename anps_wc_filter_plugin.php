9c0d3b0f0e4b1e44f537efdab962b0286fff7a5a
dusanbre <dusan.antonijevic2404@gmail.com>
2021-02-28 16:52:07 +0100

    added css and js to wordpress actions

--- anps_wc_filter_plugin.php
+++ anps_wc_filter_plugin.php
@@ -14,13 +14,19 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 require_once 'widgets/wc_ajax_filter.php';
 
+function anps_enqueue_plugin_assets_front() {
+	wp_enqueue_script( 'anps-widget-plugin-frontend', plugin_dir_url( __FILE__ ) . 'js/main.js', array( 'jquery' ), true );
+	wp_enqueue_style( 'anps-widget-style-frontend', plugin_dir_url( __FILE__ ) . 'css/main.css', array(), '1.0', false );
+}
+
+function anps_init_widget_plugin() {
+	register_widget( 'Anps_WC_Ajax_Filter_Widget' );
+}
+
 if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
-	function anps_init_widget_plugin() {
-		register_widget( 'Anps_WC_Ajax_Filter_Widget' );
-	}
 	add_action( 'widgets_init', 'anps_init_widget_plugin' );
+	add_action( 'wp_enqueue_scripts', 'anps_enqueue_plugin_assets_front' );
 } else {
 	echo 'WooCommerce is not Active.';
 }
 
-// Radi
